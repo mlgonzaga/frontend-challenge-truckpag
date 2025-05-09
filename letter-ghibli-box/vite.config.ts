914373923c37e8016973fd80eb0d3f68ac831fd3
Mateus Luiz Gonzaga <mateusluizgonzaga@gmail.com>
2025-05-09 14:21:26 -0300

    fix: correção de formatação no arquivo de configuração do Vite

--- letter-ghibli-box/vite.config.ts
+++ letter-ghibli-box/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
@@ -10,5 +11,5 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+  }
 })
