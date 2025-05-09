19d25fc8bc7bc2f60b6e323d3d572771bbc0169c
Mateus Luiz Gonzaga <mateusluizgonzaga@gmail.com>
2025-05-09 10:01:26 -0300

    add logo inspired by letterboxd

--- letter-ghibli-box/src/components/Header.tsx
+++ letter-ghibli-box/src/components/Header.tsx
@@ -1,9 +1,20 @@
 export default function Header() {
   return (
-    <div className="flex flex-col text-center gap-3">
-      <h1 className=" font-bold text-5xl">Studio Ghibli Collection</h1>
+    <div className='flex w-full flex-col gap-3 bg-slate-700 text-center text-white pb-3'>
+      <div className='mx-auto flex items-center gap-30'>
+        <div className='relative h-20 w-20'>
+          <div className='absolute left-0 h-20 w-20 rounded-full bg-orange-500 mix-blend-screen'></div>
 
-      <p className="text-lg" >
+          <div className='absolute left-12 h-20 w-20 rounded-full bg-green-500 mix-blend-screen'></div>
+
+          <div className='absolute left-24 h-20 w-20 rounded-full bg-cyan-500 mix-blend-screen'></div>
+        </div>
+        <h2 className='font-bold text-lg'>Letter Ghibli Boxd</h2>
+      </div>
+
+      <h1 className='text-5xl font-bold'>Studio Ghibli Collection</h1>
+
+      <p className='text-lg'>
         Explore the magical world of Studio Ghibli films. Mark your favorites
         and keep track of what you've watched.
       </p>
