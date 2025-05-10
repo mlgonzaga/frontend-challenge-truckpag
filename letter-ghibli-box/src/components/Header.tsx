e2e70a15a9b8c57b6e4e1d4b62308a98f34b280d
Mateus Luiz Gonzaga <mateusluizgonzaga@gmail.com>
2025-05-10 14:08:08 -0300

    style: update Header component layout and size of decorative elements for improved visual consistency

--- letter-ghibli-box/src/components/Header.tsx
+++ letter-ghibli-box/src/components/Header.tsx
@@ -1,13 +1,13 @@
 export default function Header() {
   return (
     <div className='flex w-full flex-col gap-3 bg-slate-700 text-center text-white pb-3 px-2'>
-      <div className='mx-auto flex items-center gap-30'>
-        <div className='relative h-20 w-20'>
-          <div className='absolute left-0 h-20 w-20 rounded-full bg-orange-500 mix-blend-screen'></div>
+      <div className='mx-auto flex items-center gap-15'>
+        <div className='relative h-5 w-5'>
+          <div className='absolute left-0 h-5 w-5 rounded-full bg-orange-500 mix-blend-screen'></div>
 
-          <div className='absolute left-12 h-20 w-20 rounded-full bg-green-500 mix-blend-screen'></div>
+          <div className='absolute left-3 h-5 w-5 rounded-full bg-green-500 mix-blend-screen'></div>
 
-          <div className='absolute left-24 h-20 w-20 rounded-full bg-cyan-500 mix-blend-screen'></div>
+          <div className='absolute left-6 h-5 w-5 rounded-full bg-cyan-500 mix-blend-screen'></div>
         </div>
         <h2 className='font-bold text-base sm:text-lg'>Letter Ghibli Boxd</h2>
       </div>
